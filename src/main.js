36881242ec090c566dba86db3ee63fc8f6b38ef1
VanNhuan <vannhuan404@gmail.com>
2023-03-30 20:12:04 +0700

    init common

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+
+createApp(App)
+    .use(antd)
+    .use(store)
+    .use(router)
+    .mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <CommonButton/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CommonButton from "@/components/CommonButton";
+
+export default {
+  name: 'HomeView',
+  components: {
+    CommonButton
+  }
+}
+</script>
