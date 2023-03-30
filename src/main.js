6cb14b565bbdf1667f95847a2a97b7b5197b3196
VanNhuan <vannhuan404@gmail.com>
2023-03-30 21:50:00 +0700

    update routes

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import routes from './router'
 import store from './store'
 import antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
+import {createRouter, createWebHashHistory} from "vue-router";
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
 
 createApp(App)
     .use(antd)
